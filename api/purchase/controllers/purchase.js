5992805bfeab2784e6d63b3c0903dc82b2ebc0a4
Michael Musil <infi.pc@gmail.com>
2021-09-16 15:51:02 +0200

    working transaction

--- api/purchase/controllers/purchase.js
+++ api/purchase/controllers/purchase.js
@@ -5,4 +5,22 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    async create(ctx) {
+
+        return await strapi.connections.default.transaction(async transacting => {
+            const amountToTake = ctx.request.body.Amount
+            const purchaseResponse = await strapi.query("purchase").create(ctx.request.body, {transacting});
+
+            // const editRes = await strapi.plugins['users-permissions'].services.user.edit({ id: 1 }, { credit: 333 }, {});
+            const user = await strapi.query('user', 'users-permissions').findOne({ id: 1 }, null, {transacting});
+            const availableCredit = user.Credit
+            if (availableCredit < amountToTake) {
+                throw new Error("Not enough credits")
+            }
+            await strapi.query('user', 'users-permissions').update({ id: 1 }, { Credit: availableCredit - amountToTake }, {transacting});
+
+            return purchaseResponse
+        });
+    },
+};
